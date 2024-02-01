09ce96cbfd8513df345dbc9de5c1186184fea8d3
Harry <harryedwards080@gmail.com>
2024-01-31 19:18:26 -0700

    lab3 commit

---
+++ Lab3/python_basics_harry_edwards.py
@@ -0,0 +1,48 @@
+#here's a comment
+
+print("hello world")
+
+blah = 45
+Blah = 12
+print(blah)
+print(Blah)
+blah+= Blah
+print(blah)
+
+print(type(blah))
+
+aye = 5
+bye = 6
+print (aye+bye)
+
+this = "hello "
+this2 = "my name is inigo montoya " 
+this3 = "you kill my father "
+print(this + this2 + this3 + "prepare to die")
+
+
+print(5==6)
+
+
+def subtract_numbers (a,b):
+  output = a-b
+  return output
+
+
+c = 10
+d = 15
+
+print(subtract_numbers(c,d))
+
+
+
+print(subtract_numbers(a=7,b=3))
+
+name = "Harry"
+
+def say_getouttahere ():
+  name = "Terry"
+  print("Getouttahere " + name)
+
+say_getouttahere()
+
---
+++ Lab3/python_expressions_controls.py
@@ -0,0 +1,4 @@
+print(f"boolean A: {35>90}")
+print(f"boolean B: {45==45}")
+
+print ("one is equal to 2: ", int(1==2))
